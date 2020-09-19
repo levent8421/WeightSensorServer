ea46a9b05bc4b929c0ee406562fb262a336b595a
levent8421 <levent8421@outlook.com>
2020-09-19 14:30:17 +0800

    0.1.18正式版本发布

--- database/upgrade_scada_wsa_6.sql
+++ database/upgrade_scada_wsa_6.sql
@@ -7,10 +7,10 @@ insert into t_application_config (name, value, create_time, update_time, deleted
 values ('application.db_version_name', '0.1.1', now(), now(), false);
 
 alter table t_temperature_humidity_sensor
-    add column max_temperature decimal(5, 5) not null comment 'Max Temperature',
-    add column min_temperature decimal(5, 5) not null comment 'Min Temperature',
-    add column max_humidity decimal(5, 5) not null comment 'Max humidity',
-    add column min_humidity decimal(5, 5) not null comment 'Min humidity';
+    add column max_temperature decimal(6, 3) not null comment 'Max Temperature',
+    add column min_temperature decimal(6, 3) not null comment 'Min Temperature',
+    add column max_humidity decimal(6, 3) not null comment 'Max humidity',
+    add column min_humidity decimal(6, 3) not null comment 'Min humidity';
 
 # update db_version
 update t_application_config
