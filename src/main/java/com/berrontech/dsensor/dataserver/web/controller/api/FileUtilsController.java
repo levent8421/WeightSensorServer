2d8e715edc5665cf4e6e58e2478379d0c893778c
levent8421 <levent8421@outlook.com>
2020-12-17 20:24:40 +0800

    USb device id

--- src/main/java/com/berrontech/dsensor/dataserver/web/controller/api/FileUtilsController.java
+++ src/main/java/com/berrontech/dsensor/dataserver/web/controller/api/FileUtilsController.java
@@ -1,15 +1,17 @@
 package com.berrontech.dsensor.dataserver.web.controller.api;
 
+import com.berrontech.dsensor.dataserver.common.entity.DeviceConnection;
+import com.berrontech.dsensor.dataserver.common.exception.BadRequestException;
+import com.berrontech.dsensor.dataserver.service.general.DeviceConnectionService;
 import com.berrontech.dsensor.dataserver.web.controller.AbstractController;
 import com.berrontech.dsensor.dataserver.web.vo.GeneralResult;
+import com.berrontech.dsensor.dataserver.weight.serial.util.SerialDeviceUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Create By Levent8421
@@ -25,6 +27,12 @@ import java.io.IOException;
 @RequestMapping("/api/file")
 @Slf4j
 public class FileUtilsController extends AbstractController {
+    private final DeviceConnectionService deviceConnectionService;
+
+    public FileUtilsController(DeviceConnectionService deviceConnectionService) {
+        this.deviceConnectionService = deviceConnectionService;
+    }
+
     @PostMapping("/_info")
     public GeneralResult<?> fileInfo(@RequestParam("filename") String filename) throws IOException {
         final File file = new File(filename);
@@ -32,5 +40,16 @@ public class FileUtilsController extends AbstractController {
         final String f2 = file.getAbsolutePath();
         log.debug("[{}]/[{}]", f1, f2);
         return GeneralResult.ok(f1);
+    }
+
+    @GetMapping("/_connection-id")
+    public GeneralResult<String> connectionId(@RequestParam("id") Integer id) throws IOException {
+        final DeviceConnection connection = deviceConnectionService.require(id);
+        if (!Objects.equals(connection.getType(), DeviceConnection.TYPE_SERIAL)) {
+            throw new BadRequestException("This connection is not a serial connection!");
+        }
+        final String deviceName = connection.getTarget();
+        final String usbId = SerialDeviceUtils.getUsbTtyDeviceId(deviceName);
+        return GeneralResult.ok(usbId);
     }
 }
---
+++ src/main/java/com/berrontech/dsensor/dataserver/weight/serial/util/SerialDeviceUtils.java
@@ -0,0 +1,53 @@
+package com.berrontech.dsensor.dataserver.weight.serial.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * Create By Levent8421
+ * Create Time: 2020/12/17 20:11
+ * Class Name: SerialDeviceUtils
+ * Author: Levent8421
+ * Description:
+ * 串口设备工具类
+ *
+ * @author Levent8421
+ */
+public class SerialDeviceUtils {
+    private static final File DEVICE_ID_PATH_FILE = new File("/dev/serial/by_id");
+
+    private static boolean hasUsbTtyDevice() {
+        return DEVICE_ID_PATH_FILE.exists();
+    }
+
+    /**
+     * 获取USB串口设备ID
+     *
+     * @param deviceFile device file
+     * @return id
+     * @throws IOException ioe
+     */
+    public static String getUsbTtyDeviceId(String deviceFile) throws IOException {
+        if (!hasUsbTtyDevice()) {
+            return null;
+        }
+        final File[] ids = DEVICE_ID_PATH_FILE.listFiles();
+        if (ids == null || ids.length <= 0) {
+            return null;
+        }
+        final File device = new File(deviceFile);
+        if (!device.exists()) {
+            return null;
+        }
+        final String filename = device.getName();
+        for (File linkFile : ids) {
+            final Path readPath = linkFile.toPath().toRealPath();
+            final String sourceName = readPath.toFile().getName();
+            if (filename.equalsIgnoreCase(sourceName)) {
+                return linkFile.getName();
+            }
+        }
+        return null;
+    }
+}
